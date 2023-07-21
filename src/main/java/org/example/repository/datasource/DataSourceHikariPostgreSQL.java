770fa191d450450c61ae9a25d8939653363786e4
dkanapelka <dkonopelko748@gmail.com>
2023-07-21 14:20:53 +0300

    add database connection to properties

--- src/main/java/org/example/repository/datasource/DataSourceHikariPostgreSQL.java
+++ src/main/java/org/example/repository/datasource/DataSourceHikariPostgreSQL.java
@@ -7,31 +7,35 @@ import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.sql.DataSource;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Properties;
 
 
 @Slf4j
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class DataSourceHikariPostgreSQL {
 
-    final static String USER = "db_user";
-    final static String PASSWORD = "db_password";
-    final static String URL = "jdbc:postgresql://localhost:5432/shop";
-
     private static final HikariConfig config = new HikariConfig();
     private static final HikariDataSource dataSource;
 
     static {
-        config.setJdbcUrl(URL);
-        config.setUsername(USER);
-        config.setPassword(PASSWORD);
-
-
+        Properties properties = new Properties();
+        try {
+            FileInputStream in = new FileInputStream(System.getProperty("application.properties"));
+            properties.load(in);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        config.setJdbcUrl(properties.getProperty("database.url"));
+        config.setUsername(properties.getProperty("database.username"));
+        config.setPassword(properties.getProperty("database.password"));
         config.setDriverClassName(org.postgresql.Driver.class.getName());
-        config.setConnectionTimeout(15000); //ms
-        config.setIdleTimeout(60000); //ms
-        config.setMaxLifetime(600000);//ms
+        config.setConnectionTimeout(15000);
+        config.setIdleTimeout(60000);
+        config.setMaxLifetime(600000);
         config.setAutoCommit(false);
         config.setMinimumIdle(5);
         config.setMaximumPoolSize(10);
@@ -40,7 +44,6 @@ public class DataSourceHikariPostgreSQL {
         config.addDataSourceProperty("cachePrepStmts", "true");
         config.addDataSourceProperty("prepStmtCacheSize", "250");
         config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
-
         dataSource = new HikariDataSource(config);
     }
 
